715acf4a038ce9f544b2eae22bb38719fad0fe7c
curtis <laurencechadwell@gmail.com>
2020-07-21 18:52:40 -0500

    Done.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,8 @@
 class Person < ActiveRecord::Base
   has_many :addresses
+  def addresses_attributes=(address)
+    self.address = Person.find_or_create_by(name: address[:name])
+    self.address.update(address)
+  end
 
 end
